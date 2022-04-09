4ea292dbe8af61ff10fac307709258f40ee76850
Marc Vivas <vivasmarc001@gmail.com>
2022-04-09 11:31:53 +0200

    Implemented loadEnvelopeLocations in EnvelopeWorldEnv.java

--- src/main/java/apryraz/eworld/EnvelopeWorldEnv.java
+++ src/main/java/apryraz/eworld/EnvelopeWorldEnv.java
@@ -1,6 +1,15 @@
 package apryraz.eworld;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.lang.System.exit;
 
 
 public class EnvelopeWorldEnv {
@@ -8,6 +17,10 @@ public class EnvelopeWorldEnv {
      * world dimension
      **/
     int WorldDim;
+    /**
+     * Location of the envelopes
+     */
+    Set<Position> envelopesPositions;
 
 
     /**
@@ -19,17 +32,41 @@ public class EnvelopeWorldEnv {
     public EnvelopeWorldEnv(int dim, String envelopeFile) {
 
         WorldDim = dim;
+        envelopesPositions = new HashSet<>();
         loadEnvelopeLocations(envelopeFile);
     }
 
     /**
-     * Load the list of pirates locations
+     * Load the set of envelopes locations
      *
      * @param envelopeFile name of the file that should contain a
      *                     set of envelope locations in a single line.
-     **/
+     *
+     *
+     * */
     public void loadEnvelopeLocations(String envelopeFile) {
+        String[] envelopesList;
+        String envelopes = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(envelopeFile));
+            System.out.println("ENVELOPES FILE OPENED ...");
+            envelopes = br.readLine();
+            br.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("MSG.   => Envelopes file not found");
+            exit(1);
+        } catch (IOException ex) {
+            Logger.getLogger(EnvelopeWorldEnv.class.getName()).log(Level.SEVERE, null, ex);
+            exit(2);
+        }
+        envelopesList = envelopes.split(" ");
 
+        // Add the positions into the set
+        for(int i = 0; i < envelopesList.length; i++){
+            int xPos = Integer.parseInt(String.valueOf(envelopesList[i].charAt(0)));
+            int yPos = Integer.parseInt(String.valueOf(envelopesList[i].charAt(2)));
+            envelopesPositions.add(new Position(xPos, yPos));
+        }
 
     }
 
@@ -47,12 +84,8 @@ public class EnvelopeWorldEnv {
 
         msg.showMessage();
         if (msg.getComp(0).equals("moveto")) {
-            int nx = Integer.parseInt(msg.getComp(1));
-            int ny = Integer.parseInt(msg.getComp(2));
-
-            if (withinLimits(nx, ny)) {
-
-
+            Position nextPos = new Position(Integer.parseInt(msg.getComp(1)), Integer.parseInt(msg.getComp(2)));
+            if (withinLimits(nextPos)) {
                 ans = new AMessage("movedto", msg.getComp(1), msg.getComp(2), "");
             } else
                 ans = new AMessage("notmovedto", msg.getComp(1), msg.getComp(2), "");
@@ -74,13 +107,12 @@ public class EnvelopeWorldEnv {
      * Check if position x,y is within the limits of the
      * WorldDim x WorldDim   world
      *
-     * @param x x coordinate of agent position
-     * @param y y coordinate of agent position
+     * @param pos x,y coordinate
      * @return true if (x,y) is within the limits of the world
      **/
-    public boolean withinLimits(int x, int y) {
+    public boolean withinLimits(Position pos) {
 
-        return (x >= 1 && x <= WorldDim && y >= 1 && y <= WorldDim);
+        return (pos.getX() >= 1 && pos.getX() <= WorldDim && pos.getY() >= 1 && pos.getY() <= WorldDim);
     }
 
 }
--- src/main/java/apryraz/eworld/Position.java
+++ src/main/java/apryraz/eworld/Position.java
@@ -11,4 +11,30 @@ public class Position {
         x = row;
         y = col;
     }
+
+    @Override
+    public boolean equals(Object o){
+        // If the object is compared with itself then return true
+        if (o == this) {
+            return true;
+        }
+
+        /* Check if o is an instance of Position or not */
+        if (!(o instanceof Position)) {
+            return false;
+        }
+
+        // Cast o to Position so that we can compare data members
+        Position c = (Position) o;
+
+        // Compare the data and return accordingly
+        return x == c.getX() && y == c.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
 }
